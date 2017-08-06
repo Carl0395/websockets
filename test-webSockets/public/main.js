2c4c74faa6f98bb68efccdad7c0800428962c237
Carlos <arturo0395@gmail.com>
2017-08-06 16:59:20 -0500

    correct bugs

--- test-webSockets/public/main.js
+++ test-webSockets/public/main.js
@@ -2,6 +2,32 @@
 en http://localhost:8080 y escuchamos el evento messages.*/
 var socket = io.connect('http://localhost:8080', { 'forceNew':true })
 
-socket.on('messages', function (data) {
-    console.log(data)
+addMessage = (e) => {
+  //alert("haber");
+  var message = {
+    author: document.getElementById('username').value,
+    text: document.getElementById('texto').value
+  };
+
+  socket.emit('new-message', message);
+  return false;
+}
+
+
+render = (data) => {
+    var html = data.map((elem, index) => {
+        return(`<div>
+                  <strong>${elem.author}</strong>:
+                  <em>${elem.text}</em>
+                </div>`
+        );
+    }).join(" ")
+
+    document.getElementById('messages').innerHTML = html;
+}
+
+// Este metodo es llamado cuando se recibe un mensaje por sockets
+socket.on('Messages', (data) => {
+  console.log(data)
+  render(data);
 })
--- test-webSockets/server/main.js
+++ test-webSockets/server/main.js
@@ -1,29 +1,43 @@
+// Importacion de express
 var express = require('express');
+// Ejecutar la libreria express
 var app = express();
+// Creacion del servidor
 var server = require('http').Server(app);
 
 // Servidor WebSockets
 var io = require('socket.io')(server);
 
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+  res.status(200).send("Hola mundo!")
+})
+
 /*Array de mensajes que se enviaran cuando se conecten clientes al servidor*/
 var messages = [{
     author: "Carlos",
-    text: "Hola! que tal?"
-},{
-    author: "Pepe",
-    text: "Muy bien y tu?"
-},{
-    author: "Paco",
-    text: "Genia!!"
+    text: "Hola! que tal?",
 }];
 
 /*Funcion que permite que el servidor websockets este atento a que se realice una conexion de un cliente*/
-io.on('connection', function (socket) {
+io.on('connection', (socket) => {
     console.log("Un cliente se ha conectado");
     socket.emit('Messages', messages);
+
+
+    socket.on('new-message', (data) => {
+        messages.push(data);
+
+        // con io.socket.emit se notifica a todos los usuarios conectados
+        io.sockets.emit('Messages', messages);
+    })
 })
+
 
 
 server.listen(8080, function () {
    console.log("Servidor corriendo en http://localhost:8080")
 });
+
+// Permite recibir mensajes por sockets
